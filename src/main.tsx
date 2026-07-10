8510b4511141c642c559af8d43f4f3d34f22dfeb
freetools4u <swipetoconnect@gmail.com>
2026-07-10 16:43:15 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
+import App from 'https://freetools4u.github.io/the-skill-matcher/App.tsx';
+import 'https://freetools4u.github.io/the-skill-matcher/index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
